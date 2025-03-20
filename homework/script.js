6c9c840ab4c36cc4f8e1dfc9f845f0e675c5ce36
Ilan Gustav Veras <ilanbarrosveras3@gmail.com>
2025-03-20 09:57:22 -0400

    file for script created

---
+++ homework/script.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+const formInfo = document.getElementById("contactForm")
+
+formInfo.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log("Teste")
+    const firstName = document.getElementById("firstName").value
+    const lastName = document.getElementById("lastName").value
+    const phoneNumber = document.getElementById("phoneNumber").value
+    const messageForm = document.getElementById("messageForm").value
+    alert(`${firstName} ${lastName} \n${phoneNumber}\n${messageForm}`)
+})
